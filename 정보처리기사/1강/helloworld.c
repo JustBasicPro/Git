a917d6bd84b9192247ff29382f653afad78cda97
ParkJinSeok <pjsjja458@naver.com>
2024-01-24 09:42:42 +0900

    ignore추가 해봄

---
+++ 정보처리기사/1강/helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Standard input and Output Library 의약자로 입/출력을 실행하기위한 헤더
+
+int main(void){
+printf("Hello world");
+int a;
+if(a = 1)
+a = scanf("My number is");
+return 0;
+}
+
